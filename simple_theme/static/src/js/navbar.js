dc78c2977f11e240941b11d87d08c55e6eaa024f
shaojun <453587004@qq.com>
2023-12-11 13:55:18 +0800

    添加菜单webIconData空值判断

--- simple_theme/static/src/js/navbar.js
+++ simple_theme/static/src/js/navbar.js
@@ -1,11 +1,11 @@
 /** @odoo-module */
 
-import { patch } from '@web/core/utils/patch';
-import { NavBar } from "@web/webclient/navbar/navbar";
-import { SimpleThemeDropdownItem } from './dropdown_item'
-import { SimpleThemeDropdown } from './dropdown'
+import {patch} from '@web/core/utils/patch';
+import {NavBar} from "@web/webclient/navbar/navbar";
+import {SimpleThemeDropdownItem} from './dropdown_item'
+import {SimpleThemeDropdown} from './dropdown'
 
-patch(NavBar,  {
+patch(NavBar, {
     template: "simple_theme.NavBar",
     components: {
         ...NavBar.components,
@@ -17,12 +17,17 @@ patch(NavBar,  {
 patch(NavBar.prototype, {
 
     getApps() {
-        const apps =  this.menuService.getApps();
-        const newApps = apps.map(function(item){
-            const prefix = item.webIconData.startsWith('P') ? 'data:image/svg+xml;base64,' : 'data:image/png;base64,';
-            const webIconData  = item.webIconData.startsWith('data:image') ? item.webIconData : prefix + item.webIconData.replace(/\s/g, '');
-            item['webIconData'] = webIconData;
-            return item
+        const apps = this.menuService.getApps();
+        const newApps = apps.map(function (item) {
+            if (item.webIconData) {
+                const prefix = item.webIconData.startsWith('P') ? 'data:image/svg+xml;base64,' : 'data:image/png;base64,';
+                const webIconData = item.webIconData.startsWith('data:image') ? item.webIconData : prefix + item.webIconData.replace(/\s/g, '');
+                item['webIconData'] = webIconData;
+            }else {
+                item['webIconData'] = '';
+            }
+
+            return item;
 
         })
         return newApps
