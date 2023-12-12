59bcdd79ba850a33e5a926f3e00a4c258d4c559f
shaojun666 <453587004@qq.com>
2023-12-12 21:44:40 +0800

    添加web_ext模块，扩展UserMenu,去掉部分用户菜单

---
+++ web_ext/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Web Extend",
+    "summary": "Web Extend",
+    "author": "shaojun",
+    "website": "https://odooiot.com",
+    "category": "Tools",
+    "version": "16.0.0.1",
+    "depends": ["base", "web"],
+    "data": [
+
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'web_ext/static/src/js/user_menu.js',
+        ],
+    },
+    'bootstrap': True,
+}
