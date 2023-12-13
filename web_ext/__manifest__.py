84e0822c0d6cdeb593c82bc3019301969c4d1985
shaojun666 <453587004@qq.com>
2023-12-13 14:47:29 +0800

    修改登录页面

--- web_ext/__manifest__.py
+++ web_ext/__manifest__.py
@@ -7,11 +7,12 @@
     "category": "Tools",
     "version": "15.0.0.1",
     "depends": ["base", "web"],
+    'data': [
+        "views/webclient_templates.xml",
+    ],
     'assets': {
         'web.assets_backend': [
             'web_ext/static/src/js/user_menu.js',
-
-
         ]
     },
     'bootstrap': True,
